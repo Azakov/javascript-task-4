93b3bf9d48de696d6db82dfc8bac46b0d3f2f973
Krivedina <azakov31@gmail.com>
2018-11-06 06:38:49 +0500

    Update emitter.js

--- emitter.js
+++ emitter.js
@@ -10,7 +10,24 @@ const isStar = true;
  * Возвращает новый emitter
  * @returns {Object}
  */
+
 function getEmitter() {
+    const events = {};
+
+    function getEvents(event) {
+        let result = [];
+        let eventParse = event.split('.');
+        while (eventParse.length > 0) {
+            let eventName = eventParse.join('.');
+            if (events[eventName]) {
+                result.push(eventName);
+            }
+            eventParse.pop();
+        }
+
+        return result;
+    }
+
     return {
 
         /**
@@ -18,26 +35,44 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!events.hasOwnProperty(event)) {
+                events[event] = new Map();
+            }
+            events[event].set(context, handler);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            Object.keys(events)
+                .filter(eventName =>
+                    !eventName.indexOf(event) ||
+                    !eventName.indexOf(event + '.') ? events[eventName].delete(context) : eventName
+                );
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            getEvents(event)
+                .forEach(eventName => events[eventName]
+                    .forEach((handler, context) => handler.call(context)));
+
+            return this;
         },
 
         /**
@@ -47,9 +82,18 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object}
          */
         several: function (event, context, handler, times) {
             console.info(event, context, handler, times);
+            this.on(event, context, () => {
+                if (times > 0) {
+                    handler.call(context);
+                    times--;
+                }
+            });
+
+            return this;
         },
 
         /**
@@ -59,12 +103,23 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object}
          */
         through: function (event, context, handler, frequency) {
             console.info(event, context, handler, frequency);
+            let callsCount = 0;
+            this.on(event, context, () => {
+                if (Number.isInteger(callsCount / frequency)) {
+                    handler.call(context);
+                }
+                callsCount++;
+            });
+
+            return this;
         }
     };
 }
+
 
 module.exports = {
     getEmitter,
